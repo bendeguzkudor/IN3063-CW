5f61fb6f4843a4e1ab803855be07b79df3d6f4a4
Ben <kudorbedi@gmail.com>
2024-12-17 17:22:55 +0100

    wip: cnn model

--- src/task2/cifar_cnn.py
+++ src/task2/cifar_cnn.py
@@ -1,6 +1,8 @@
 import torchvision
 from torchvision.transforms import transforms
 import torch
+import torch.nn.functional as F
+import torch.nn as nn
 
 torch.manual_seed(42)
 
@@ -25,3 +27,19 @@ print(train_dataset[0])
 classes = ('airplane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)  
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1) 
+        self.pool = nn.MaxPool2d(2, 2)                           #  pooling layer
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)                   # fully connected layer
+        self.fc2 = nn.Linear(128, 10)                           # output layer 10
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # conv1 -> ReLU -> Pool
+        x = self.pool(F.relu(self.conv2(x)))  # conv2 -> ReLU -> Pool
+        x = x.view(-1, 64 * 8 * 8)            # flatten the output
+        x = F.relu(self.fc1(x))               # fully connected -> ReLU
+        x = self.fc2(x)                       # fully connected -> out
+        return x
