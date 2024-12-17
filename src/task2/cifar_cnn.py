88bff70b54b23af0346dd692aaa60377ccdb1330
Ben <kudorbedi@gmail.com>
2024-12-17 17:03:03 +0100

    feat: getting the dataset

---
+++ src/task2/cifar_cnn.py
@@ -0,0 +1,23 @@
+import torchvision
+from torchvision.transforms import transforms
+import torch
+
+torch.manual_seed(42)
+
+
+
+# convert to nparray and normalize 
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# get dataset
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+
+print(train_dataset[0])
