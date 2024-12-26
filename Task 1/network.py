6b04dcc703862886426e5bfa9df81f731bf2a337
AyrtonCityU <ayrton.ahmet@city.ac.uk>
2024-12-26 22:14:53 +0000

    feat: NN functionality implemented with forward and backward propagation

--- Task 1/network.py
+++ Task 1/network.py
@@ -4,9 +4,9 @@ from layers.activation import ReLU, Sigmoid, Softmax
 from layers.dropout import Dropout
 
 # Checklist
-# Layer sizes parameter 
-# Units can be changed
-# Activation functions parameter
+# Layer sizes parameter DONE
+# Units can be changed DONE
+# Activation functions parameter DONE
 # Adjustable dropout_rate
 # Regulariser (L1 or L2)
 
@@ -15,23 +15,46 @@ class NeuralNetwork:
         # Store network layers here
         self.layers = []
 
-        # Dense layers
-        # Activation functions between layers
-        # Final layer softmax for classification
+ # Build network architecture by adding layers one by one
+        for i in range(len(layer_sizes)-1):
+            # Add a dense layer connecting current layer size to next layer size
+            self.layers.append(Dense(layer_sizes[i], layer_sizes[i+1]))
+            
+            # Add appropriate activation function
+            # For the output layer (last layer), we use Softmax for classification
+            # For hidden layers, we use the activation function provided
+            if i < len(layer_sizes)-2:
+                self.layers.append(activations[i])
+            else:
+                self.layers.append(Softmax())
+
 
     def forward (self, X):
-        # Input (X) passes through each layer
-
-        pass
+        current = x
+        # Input X through each layer in order
+        for layer in self.layers:
+            current = layer.forward(current)
+        return current
 
     def backward(self, grad):
         # Calculate gradient for each layer
         # Store parameters and gradients for optimisation
+        params, grads = [], []
 
-        pass
+        #Backward propagation through the network, in reverse order
+        for layer in reversed(self.layers):
+            # Propagate the gradient through current layer
+            grad = layer.backward(grad)
+            # Collect parameters and gradients from dense layers only
+            if isinstance(layer, Dense):
+                params.extend([layer.weights, layer.bias])
+                grads.extend([layer.weights_grad, layer.bias_grad])
+        return params, grads
     
     def train_step(self, X, y):
         # Forward pass -> loss gradient -> backward pass
+        predictions = self.forward(X)
+        grad = predictions - y
+        params, grads = self.backward(grad)
 
-        pass
-        
+        return predictions
